3c82655868c6d6a607ec282a033b46645a7a8e99
AjaySatpati123 <ajaysatpati9@gmail.com>
2024-01-14 19:27:51 +0530

    This is my First Commit

--- app/src/main/java/com/example/zhoppi/FormActivity.kt
+++ app/src/main/java/com/example/zhoppi/FormActivity.kt
@@ -21,22 +21,29 @@ class FormActivity : AppCompatActivity() {
         val intent = intent
         val receivedData = intent.getStringExtra("check")
         Toast.makeText(this, receivedData, Toast.LENGTH_SHORT).show()
-        if(receivedData == "1" ){
-            findViewById<EditText>(R.id.shopName).visibility = View.GONE
-            findViewById<EditText>(R.id.shopStreet).visibility = View.GONE
-            findViewById<EditText>(R.id.shopCity).visibility = View.GONE
-            findViewById<EditText>(R.id.shopState).visibility = View.GONE
-            findViewById<EditText>(R.id.pincode).visibility = View.GONE
-            findViewById<EditText>(R.id.shopGstin).visibility = View.GONE
-            val letsGoButton = findViewById<Button>(R.id.letsGo)
-            val layoutParams = letsGoButton.layoutParams as ConstraintLayout.LayoutParams
-            layoutParams.verticalBias = 0.5f
-            letsGoButton.layoutParams = layoutParams
-            letsGoButton.setOnClickListener {
-                val sName = findViewById<EditText>(R.id.name).text.toString()
-                val sContactNo = findViewById<EditText>(R.id.contactNo).text.toString()
-                val sUserName = findViewById<EditText>(R.id.username).text.toString()
-                val sType = "buyer"
+        if (receivedData == "1") {
+            buyer()
+        } else {
+            seller()
+        }
+    }
+    private fun buyer(){
+        findViewById<EditText>(R.id.shopName).visibility = View.GONE
+        findViewById<EditText>(R.id.shopStreet).visibility = View.GONE
+        findViewById<EditText>(R.id.shopCity).visibility = View.GONE
+        findViewById<EditText>(R.id.shopState).visibility = View.GONE
+        findViewById<EditText>(R.id.pinCode).visibility = View.GONE
+        findViewById<EditText>(R.id.shopGstIn).visibility = View.GONE
+        val letsGoButton = findViewById<Button>(R.id.letsGo)
+        val layoutParams = letsGoButton.layoutParams as ConstraintLayout.LayoutParams
+        layoutParams.verticalBias = 0.5f
+        letsGoButton.layoutParams = layoutParams
+        letsGoButton.setOnClickListener {
+            val sName = findViewById<EditText>(R.id.name).text.toString()
+            val sContactNo = findViewById<EditText>(R.id.contactNo).text.toString()
+            val sUserName = findViewById<EditText>(R.id.username).text.toString()
+            val sType = "buyer"
+            if (sName.isNotEmpty() && sContactNo.isNotEmpty() && sUserName.isNotEmpty() && sType.isNotEmpty()) {
                 val userMap = hashMapOf(
                     "name" to sName,
                     "contactNo" to sContactNo,
@@ -45,15 +52,65 @@ class FormActivity : AppCompatActivity() {
                 )
                 val userId = FirebaseAuth.getInstance().currentUser!!.uid
                 db.collection("user").document(userId).set(userMap).addOnSuccessListener {
-                    Toast.makeText(this, "All set", Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this,SignInActivity::class.java))
+                    Toast.makeText(this, "Buyer Account Created", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, SignInActivity::class.java))
                     finish()
                 }.addOnFailureListener {
                     Toast.makeText(this, "Please try again", Toast.LENGTH_SHORT).show()
                 }
+            } else {
+                Toast.makeText(this, "Fill all the details", Toast.LENGTH_SHORT).show()
             }
-        }else{
+        }
+    }
+    private fun seller(){
+        val letsGoButton = findViewById<Button>(R.id.letsGo)
+        letsGoButton.setOnClickListener {
+            val sName = findViewById<EditText>(R.id.name).text.toString()
+            val sContactNo = findViewById<EditText>(R.id.contactNo).text.toString()
+            val sUserName = findViewById<EditText>(R.id.username).text.toString()
+            val sType = "seller"
 
+            val sShopName = findViewById<EditText>(R.id.shopName).text.toString()
+            val sShopStreet = findViewById<EditText>(R.id.shopStreet).text.toString()
+            val sShopCity = findViewById<EditText>(R.id.shopCity).text.toString()
+            val sShopState = findViewById<EditText>(R.id.shopState).text.toString()
+            val sPinCode = findViewById<EditText>(R.id.pinCode).text.toString()
+            val sAddress = hashMapOf(
+                "shopStreet" to sShopStreet,
+                "shopCity" to sShopCity,
+                "shopState" to sShopState,
+                "pinCode" to sPinCode
+            )
+            val sGstIn = findViewById<EditText>(R.id.shopGstIn).text.toString()
+
+            val sShopDetails = hashMapOf(
+                "shopName" to sShopName,
+                "address" to sAddress,
+                "gstIn" to sGstIn
+            )
+            if (sName.isNotEmpty() && sContactNo.isNotEmpty() && sUserName.isNotEmpty() && sType.isNotEmpty()
+                && sShopName.isNotEmpty() && sShopStreet.isNotEmpty() && sShopCity.isNotEmpty() && sShopState.isNotEmpty()
+                && sPinCode.isNotEmpty() && sGstIn.isNotEmpty()
+            ) {
+                val userMap = hashMapOf(
+                    "name" to sName,
+                    "contactNo" to sContactNo,
+                    "username" to sUserName,
+                    "type" to sType,
+                    "shopDetails" to sShopDetails
+                )
+                val userId = FirebaseAuth.getInstance().currentUser!!.uid
+                db.collection("user").document(userId).set(userMap).addOnSuccessListener {
+                    Toast.makeText(this, "Seller Account Created", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, SignInActivity::class.java))
+                    finish()
+                }.addOnFailureListener {
+                    Toast.makeText(this, "Please try again", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Fill all the details", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
